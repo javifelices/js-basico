2c28cbc40cb7e566dd1560fcfee42196423a8942
Javi Felices <javifelices@gmail.com>
2021-05-16 21:36:05 +0200

    Agregada practica funciones

---
+++ pjs03-funciones.js
@@ -0,0 +1,29 @@
+// Funciones Declarativas
+
+// function miFuncion() {
+//     return 3;
+// }
+
+// miFuncion();
+
+function saludarEstudiantes(estudiante) {
+    console.log(`Hola ${estudiante}`);
+}
+
+saludarEstudiantes('Javi');
+
+function sumar(a, b) {
+    var resultado = a + b;
+    return resultado;
+}
+
+var operacion = sumar(2, 3);
+console.log(operacion);
+
+// Funciones de Expresion
+
+// var miFuncion = function (a, b) {
+//     return a + b;
+// }
+
+// miFuncion(2, 3)
