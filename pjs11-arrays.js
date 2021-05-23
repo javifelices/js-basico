1fd025bbcf8edca834a02f5e2ce0d66158b5b1a7
Javi Felices <javifelices@gmail.com>
2021-05-23 12:59:20 +0200

    Agregada practica inicial de arrays

---
+++ pjs11-arrays.js
@@ -0,0 +1,10 @@
+var frutas = ["Manzana", "Plátano", "Cereza", "Fresa"];
+frutas.push("Uvas"); // Inserta un elemento al final. Devuelve un número
+console.log(frutas);
+frutas.pop(); // Elimina el último elemento. Devuelve un string
+console.log(frutas);
+frutas.unshift("Uvas"); // Inserta un elemento al principio. Devuelve un número
+console.log(frutas);
+frutas.shift(); // Elimina el primer elemento. Devuelve un string
+console.log(frutas);
+frutas.indexOf("Cereza"); // Devuelve un número que es la posición del elemento.
